0c1eb4a900a8340d7beebf13e82284aebc4da2ad
Roar Brænden <roar.branden@niva.no>
2019-10-24 10:30:15 +0200

    Appending Økoregion and Vanntype to PTI values

--- ProjectNaturindeks.py
+++ ProjectNaturindeks.py
@@ -37,7 +37,7 @@ def downloadVannNett(report):
             "WhereColumns": [],
             "SelectColumns": [3553, 3554, 3555, 3556, 3557, 3558, 3560, 3562, 3564, 3566, 3568, 3570, 3572, 3574, 3576, 3577, 3578]
         }
-        writer = xlsWriter("C:/Naturindeks/vann-nett-elver.xlsx")
+        writer = xlsWriter("C:/Naturindeks/Vann-nett-elver.xlsx")
     else:
         req_data = {
                 "ReportID": 109,
@@ -49,7 +49,7 @@ def downloadVannNett(report):
                     3535, 3537, 3539, 3541, 3543, 3545, 3547, 3549,
                     3550, 3551
                 ]}
-        writer = xlsWriter("C:/Naturindeks/vann-nett-sjoer.xlsx")
+        writer = xlsWriter("C:/Naturindeks/Vann-nett-sjoer.xlsx")
 
     out_frame = pd.DataFrame(columns=["VannforekomstID", "Vannforekomstnavn", "Vanntype", "Økoregion"])
     out_frame.set_index("VannforekomstID", inplace=True)
@@ -83,38 +83,70 @@ def rewriteNIVA_PTI():
     pti_df = pd.read_excel("c:/Naturindeks/Plankton.xlsx", "PlanktonParameter", header=1)
 
     attribute_df = pd.read_excel("c:/Naturindeks/Plankton.xlsx", "StationAttribute")
-    attribute_df.set_index("StationId", inplace=True)
-
+    
     point_df = pd.read_excel("c:/Naturindeks/Plankton.xlsx", "StationPoint")
 
+    vannett_df = pd.read_excel("c:/Naturindeks/Vann-nett-sjoer.xlsx", "Sheet1")
+
+    data_rows = []
     for idx, pti_row in pti_df.iterrows():
         stationid = pti_row[2]
-        attribute_row = attribute_df[stationid]
+        attribute_row = attribute_df.loc[attribute_df["StationId"] == stationid].iloc[0]
         kommune = ""
         vannforekomst = ""
-        if not attribute_row == np.nan:
-            kommune = "{:04.0f}".format(attribute_df.at[stationid, "Kommunenummer"])
-            vannforekomst = attribute_df.at[stationid, "VannforekomstID"]
+        if not attribute_row.empty:
+            kommune = "{:04.0f}".format(attribute_row["Kommunenummer"])
+            vannforekomst = attribute_row["VannforekomstID"]
+
+        okoregion = ""
+        vanntype = ""
+        if not vannforekomst == "":
+            try:
+               vannett_row = vannett_df.loc[vannett_df["VannforekomstID"] == vannforekomst].iloc[0]
+               if not vannett_row.empty:
+                  okoregion = vannett_row["Økoregion"]
+                  vanntype = vannett_row["Vanntype"]
+            except IndexError:
+                print(vannforekomst)
         point = point_df.loc[point_df["StationId"] == stationid].iloc[0]
-        print(point["Latitude"], point["Longitude"], str(pti_row[6])[0:10], pti_row[10], kommune, vannforekomst)
+        data_rows.append({"Latitude": point["Latitude"],
+                          "Longitude": point["Longitude"],
+                          "Date": str(pti_row[6])[0:10],
+                          "PTI": pti_row[10],
+                          "Kommunenr": kommune,
+                          "VannforekomstID": vannforekomst,
+                          "Økoregion": okoregion,
+                          "Vanntype": vanntype})
+
+    out_df = pd.DataFrame(data_rows, columns=["Latitude", "Longitude", "Date", "PTI", "Kommunenr", "VannforekomstID", "Økoregion", "Vanntype"])
+    writer = xlsWriter("C:/Naturindeks/Naturindeks_plankton.xlsx")
+    out_df.to_excel(writer)
+    writer.save()
 
 
 def rewriteNIVA_ASPT():
     aspt_df = pd.read_excel("c:/Naturindeks/Bunndyr.xlsx", "BunndyrVariables")
-
     attribute_df = pd.read_excel("c:/Naturindeks/Bunndyr.xlsx", "StationAttribute")
-    attribute_df.set_index("StationId", inplace=True)
-
     point_df = pd.read_excel("c:/Naturindeks/Bunndyr.xlsx", "StationPoint")
 
+    data_rows = []
     for idx, aspt_row in aspt_df.iterrows():
         stationid = aspt_row[2]
-        attribute_row = attribute_df[stationid]
+        attribute_row = attribute_df.loc[attribute_df["StationId"] == stationid].iloc[0]
         kommune = ""
         vannforekomst = ""
-        if not attribute_row == np.nan:
-            kommune = "{:04.0f}".format(attribute_df.at[stationid, "Kommunenummer"])
-            vannforekomst = attribute_df.at[stationid, "VannforekomstID"]
-        point = point_df.loc[point_df["StationId"] == stationid].iloc[0]
-        print(point["Latitude"], point["Longitude"], str(aspt_row[5])[0:10], aspt_row[6], kommune, vannforekomst)
+        if not attribute_row.empty:
+            kommune = "{:04.0f}".format(attribute_row["Kommunenummer"])
+            vannforekomst = attribute_row["VannforekomstID"]
+        point_row = point_df.loc[point_df["StationId"] == stationid].iloc[0]
+        data_rows.append({"Latitude": point_row["Latitude"],
+                       "Longitude": point_row["Longitude"],
+                       "Date": str(aspt_row[5])[0:10],
+                       "ASPT": aspt_row[6],
+                       "Kommunenr": kommune,
+                       "VannforekomstID": vannforekomst})
 
+    out_df = pd.DataFrame(data_rows, columns=["Latitude", "Longitude", "Date", "ASPT", "Kommunenr", "VannforekomstID"])
+    writer = xlsWriter("C:/Naturindeks/Naturindeks_bunndyr.xlsx")
+    out_df.to_excel(writer)
+    writer.save()
