560542492428a50460807f28b9e4247e35e19772
James Sample <james.e.sample@gmail.com>
2020-12-18 15:19:29 +0000

    Package for pip-installation

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+lib_fold = os.path.dirname(os.path.realpath(__file__))
+requirements_path = lib_fold + "/requirements.txt"
+install_requires = []
+if os.path.isfile(requirements_path):
+    with open(requirements_path) as f:
+        install_requires = f.read().splitlines()
+
+setuptools.setup(
+    name="aquamonitor",
+    version="0.0.1",
+    author="Roar Brenden",
+    author_email="roar.brenden@niva.no",
+    description="Python scripts to access Nivabasen",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NIVANorge/Aquamonitor-Python",
+    py_modules=["aquamonitor"],
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GPLv3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+)
