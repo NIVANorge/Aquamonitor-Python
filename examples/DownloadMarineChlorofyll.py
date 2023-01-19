039e36677ef7e093a1f650efd63970f9bdf86e5b
Roar Brænden <roar.branden@niva.no>
2023-01-19 09:07:40 +0100

    With additional comments

--- examples/DownloadMarineChlorofyll.py
+++ examples/DownloadMarineChlorofyll.py
@@ -1,12 +1,15 @@
-
-# station_type_id = 3
-# Water.parameter_id = 261
+# Find some Id's from NIVADATABASE
+#
+# From station_types :  station_type_id = 3
+#
+# From wc_parameter_definitions : Water.parameter_id = 261
 
 import aquamonitor as am
 import datetime
-# am.host = "https://test-aquamonitor.niva.no/"
-token = am.login()
 
+# We need some additional functions
+#
+#  Start generating Excel file at the server
 def make_excel(title, stationids, where):
     expires = datetime.date.today() + datetime.timedelta(days=1)
     data = {
@@ -18,7 +21,7 @@ def make_excel(title, stationids, where):
     resp = am.createDatafile(token, data)
     return resp["Id"]
 
-
+#  Check if the file is ready and download to path
 def download_file(id, filename, path):
     archived = False
     while not archived:
@@ -26,8 +29,8 @@ def download_file(id, filename, path):
         archived = resp.get("Archived")
     am.downloadArchive(token, id, filename, path)
 
+token = am.login()
 where = "station_type_id = 3 and Water.parameter_id = 261"
-
 stids = am.Query(where).list()
 fil = make_excel("Klfa", stids, where)
 download_file(fil, "Klfa.xlsx", "C:/Temp/Klfa.xlsx")
