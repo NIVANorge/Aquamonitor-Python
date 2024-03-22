8be7030e0148be81394cd58fd1a616c53db9ef3d
Roar Brænden <roar.branden@niva.no>
2024-03-22 15:05:27 +0100

    Update version 0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if os.path.isfile(requirements_path):
 
 setuptools.setup(
     name="aquamonitor",
-    version="0.2",
+    version="0.3",
     author="Roar Brænden",
     author_email="roar.branden@niva.no",
     description="Python scripts to access Nivabasen",
@@ -23,7 +23,7 @@ setuptools.setup(
     py_modules=["aquamonitor"],
     packages=setuptools.find_packages(),
     install_requires=install_requires,
-    python_requires=">=3.8",
+    python_requires=">=3.12",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
